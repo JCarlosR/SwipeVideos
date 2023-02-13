db8585a3d4e0744456eb9bf8bb670d4207c4f0fa
Juan Ramos <juancagb.17@gmail.com>
2023-02-13 16:23:39 -0500

    Upgrade Gradle, Update video links and texts with working public videos

--- app/src/main/java/com/lentimosystems/swipevideos/MainActivity.java
+++ app/src/main/java/com/lentimosystems/swipevideos/MainActivity.java
@@ -19,21 +19,21 @@ public class MainActivity extends AppCompatActivity {
         List<VideoItem> videoItems = new ArrayList<>();
 
         VideoItem item = new VideoItem();
-        item.videoURL = "https://liciolentimo.co.ke/img/videos/facebook.mp4";
-        item.videoTitle = "Women In Tech";
-        item.videoDesc = "International Women's Day 2019";
+        item.videoURL = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+        item.videoTitle = "Big Buck Bunny";
+        item.videoDesc = "Big Buck Bunny tells the story of a giant rabbit with a heart bigger than himself.";
         videoItems.add(item);
 
         VideoItem item2 = new VideoItem();
-        item2.videoURL = "https://liciolentimo.co.ke/img/videos/facebook3.mp4";
-        item2.videoTitle = "Sasha Solomon";
-        item2.videoDesc = "How Sasha Solomon Became a Software Developer at Twitter";
+        item2.videoURL = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4";
+        item2.videoTitle = "We Are Going On Bullrun";
+        item2.videoDesc = "The Smoking Tire is going on the 2010 Bullrun Live Rally in a 2011 Shelby GT500, and posting a video from the road every single day!";
         videoItems.add(item2);
 
         VideoItem item3 = new VideoItem();
-        item3.videoURL = "https://liciolentimo.co.ke/img/videos/facebook2.mp4";
-        item3.videoTitle = "Happy Hour Wednesday";
-        item3.videoDesc = " Depth-First Search Algorithm";
+        item3.videoURL = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4";
+        item3.videoTitle = "Tears of Steel";
+        item3.videoDesc = "Tears of Steel was realized with crowd-funding by users of the open source 3D creation tool Blender.";
         videoItems.add(item3);
 
         videosViewPager.setAdapter(new VideosAdapter(videoItems));
