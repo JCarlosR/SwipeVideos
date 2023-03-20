20f63bf57bfcfbdb64919d2250f1e7f08eae2173
Juan Ramos <juancagb.17@gmail.com>
2023-03-20 13:05:17 -0500

    Add a runnable to print the buffering % as it advances

--- app/src/main/java/com/programacionymas/swipevideos/ui/player/MyPlayer.kt
+++ app/src/main/java/com/programacionymas/swipevideos/ui/player/MyPlayer.kt
@@ -1,6 +1,8 @@
 package com.programacionymas.swipevideos.ui.player
 
 import android.content.Context
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.Player
@@ -85,14 +87,12 @@ class MyPlayer {
         exoPlayer.addListener(object : Player.Listener {
             override fun onPlaybackStateChanged(playbackState: Int) {
                 when (playbackState) {
-                    Player.STATE_IDLE -> {
-                        Log.d(TAG_STATE, "STATE_IDLE")
-                    }
                     Player.STATE_BUFFERING -> {
                         Log.d(TAG_STATE, "STATE_BUFFERING $videoUri")
                     }
                     Player.STATE_READY -> {
-                        Log.d(TAG_STATE, "STATE_READY $videoUri")
+                        Log.d(TAG_STATE, "STATE_READY $videoUri | playWhenReady ${exoPlayer.playWhenReady}")
+                        startBufferingUpdates()
                     }
                     Player.STATE_ENDED -> {
                         Log.d(TAG_STATE, "STATE_ENDED")
@@ -102,9 +102,45 @@ class MyPlayer {
         })
     }
 
+    private val mainHandler by lazy {
+        Handler(Looper.getMainLooper())
+    }
+
+    private var lastBufferPercentage: Int = 0
+
+    private fun startBufferingUpdates() {
+        mainHandler.postDelayed(bufferingRunnable, BUFFERING_UPDATES_INTERVAL)
+    }
+
+    /**
+     * Periodic updates for the buffering, until completed.
+     */
+    private val bufferingRunnable = object : Runnable {
+        override fun run() {
+            if (exoPlayer.bufferedPercentage != lastBufferPercentage) {
+                Log.d(TAG, "Buffer $videoFileName: position ${exoPlayer.bufferedPosition}, percentage ${exoPlayer.bufferedPercentage}")
+                lastBufferPercentage = exoPlayer.bufferedPercentage
+            }
+
+            if (exoPlayer.bufferedPercentage < 100) {
+                mainHandler.postDelayed(this, BUFFERING_UPDATES_INTERVAL)
+            }
+        }
+    }
+
+    private val videoFileName: String
+        get() {
+            return this.videoUri.substringAfterLast("/")
+        }
+
     companion object {
         private const val TAG = "MyPlayer"
         private const val TAG_STATE = "ExoPlayerState"
         private const val DEBUG = false
+
+        /**
+         * In milliseconds.
+         */
+        private const val BUFFERING_UPDATES_INTERVAL = 2_000L
     }
 }
